98d3431f804a13d51511339277571e54a282e82c
Greg <gregory.broche@yahoo.fr>
2024-07-11 15:28:11 +0200

    implemented creation of app main window through the use of fxml for ease of view templating

---
+++ src/main/java/com/gregorybroche/imageresolver/controller/MainController.java
@@ -0,0 +1,8 @@
+package com.gregorybroche.imageresolver.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MainController {
+    
+}
--- src/main/java/com/gregorybroche/imageresolver/event/StageReadyEventListener.java
+++ src/main/java/com/gregorybroche/imageresolver/event/StageReadyEventListener.java
@@ -1,42 +1,47 @@
 package com.gregorybroche.imageresolver.event;
 
+import java.io.IOException;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.io.Resource;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 @Component
 public class StageReadyEventListener implements ApplicationListener<StageReadyEvent>{
     private final ApplicationContext APP_CONTEXT;
-    private final String APP_NAME;
-    private final int APP_BASE_WIDTH = 1080;
-    private final int APP_BASE_HEIGHT = 720;
-    private final Color APP_BACKGROUND_COLOR = Color.valueOf("#193947");
+    private final Resource FXML;
+    private final String APP_NAME = "ReSolver";
 
-    public StageReadyEventListener(ApplicationContext applicationContext, @Value("${app.title}") String applicationName){
-        this.APP_NAME = applicationName;
+    public StageReadyEventListener(ApplicationContext applicationContext, @Value("classpath:/com/gregorybroche/imageresolver/views/main.fxml") Resource fxml){
         this.APP_CONTEXT = applicationContext;
+        this.FXML = fxml;
     }
 
     @Override
     public void onApplicationEvent(@NonNull StageReadyEvent event){
-        Stage stage = event.getStage();
-        Pane testPane = new Pane(new Label("test"));
+        try {
+            Stage stage = event.getStage();
+            URL viewUrl = this.FXML.getURL();
+            FXMLLoader loader = new FXMLLoader(viewUrl);
+            loader.setControllerFactory(this.APP_CONTEXT::getBean);
+            Parent root = loader.load();
+            Scene mainScene = new Scene(root);
+            stage.setTitle(this.APP_NAME);
+            stage.setScene(mainScene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        Group root = new Group(testPane);
-
-        Scene mainScene = new Scene(root, this.APP_BASE_WIDTH, this.APP_BASE_HEIGHT, this.APP_BACKGROUND_COLOR);
-
-        stage.setTitle(this.APP_NAME);
-        stage.setScene(mainScene);
-        stage.show();
     }
 }
