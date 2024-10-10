a9d1b7e3a39ea7562153ebca60a752c40dc27898
Greg <gregory.broche@yahoo.fr>
2024-10-10 17:20:06 +0200

    added logic for toggling save button on template form based on validation of user inputs

--- src/main/java/com/gregorybroche/imageresolver/controller/templateFormController.java
+++ src/main/java/com/gregorybroche/imageresolver/controller/templateFormController.java
@@ -1,5 +1,8 @@
 package com.gregorybroche.imageresolver.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +20,7 @@ import javafx.scene.text.Text;
 @Component
 public class TemplateFormController {
     private TemplateFormSubmitListener submitListener;
+    private Map<String, Boolean> inputValidationMap = new HashMap<String, Boolean>();
 
     @Autowired
     private TemplateFormValidatorService templateFormValidatorService;
@@ -93,6 +97,7 @@ public class TemplateFormController {
     // Populate ChoiceBox with some items
     selectFormat.getItems().addAll("PNG", "JPG", "BMP", "GIF");
     selectFormat.setValue("PNG"); // Set default value if needed
+    initializeInputValidationMap();
 }
 
 
@@ -102,9 +107,11 @@ public class TemplateFormController {
         ValidationResponse validationState = templateFormValidatorService.validateTemplateNameInput(input);
         if(!validationState.getIsSuccess()){
             showInputError(inputTemplateNameError, validationState.getMessage());
-            return;
+        }else{
+            hideInputError(inputTemplateNameError);
         }
-        hideInputError(inputTemplateNameError);
+        setInputValidationForTemplateName(validationState.getIsSuccess());
+        toggleSaveButton(areInputsValid());
     }
 
     @FXML
@@ -143,5 +150,58 @@ public class TemplateFormController {
     public void hideInputError(Text textElement){
         textElement.setVisible(false);
         textElement.setText("");
+    }
+
+    public void initializeInputValidationMap(){
+        setInputValidationForTemplateName(false);
+        setInputValidationForBaseName(false);
+        setInputValidationForPrefix(false);
+        setInputValidationForSuffix(false);
+        setInputValidationForWidth(false);
+        setInputValidationForHeight(false);
+        setInputValidationForResolution(false);
+        setInputValidationForFormat(false);
+    }
+
+    public void toggleSaveButton(boolean mustEnableButton){
+        buttonSave.setDisable(!mustEnableButton);
+    }
+
+    public boolean areInputsValid(){
+        return (
+            inputValidationMap.get("templateName")
+            // && inputValidationMap.get("baseName")
+            // && inputValidationMap.get("prefix")
+            // && inputValidationMap.get("suffix")
+            // && inputValidationMap.get("width")
+            // && inputValidationMap.get("height")
+            // && inputValidationMap.get("resolution")
+            // && inputValidationMap.get("format")
+        );
+    }
+
+    public void setInputValidationForTemplateName(boolean isInputValid){
+        inputValidationMap.put("templateName", isInputValid);
+    }
+    public void setInputValidationForBaseName(boolean isInputValid){
+        inputValidationMap.put("baseName", isInputValid);
+    }
+    public void setInputValidationForPrefix(boolean isInputValid){
+        inputValidationMap.put("prefix", isInputValid);
+    }
+    public void setInputValidationForSuffix(boolean isInputValid){
+        inputValidationMap.put("suffix", isInputValid);
+    }
+    public void setInputValidationForWidth(boolean isInputValid){
+        inputValidationMap.put("width", isInputValid);
+    }
+    public void setInputValidationForHeight(boolean isInputValid){
+        inputValidationMap.put("height", isInputValid);
+    }
+    public void setInputValidationForResolution(boolean isInputValid){
+        inputValidationMap.put("resolution", isInputValid);
+    }
+    public void setInputValidationForFormat(boolean isInputValid){
+        inputValidationMap.put("format", isInputValid);
     }
 }
